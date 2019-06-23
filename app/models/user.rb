16c8a3a7138e987dc897ca8443d898b923b088fc
Ryu Sato <ryu@weseek.co.jp>
2019-06-23 22:41:28 +0900

    rolify をインストール、初期化し、ユーザに管理者用とデフォルトのロールが設定されるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  rolify
   devise :database_authenticatable
+
+  after_create :assign_admin_role
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:guest) if self.roles.blank?
+  end
+
+  def assign_admin_role
+    self.add_role(:admin) if self.name == 'admin'
+  end
 end
